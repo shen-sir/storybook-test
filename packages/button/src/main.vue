de5adc9bc651ea41052a88727a077d8876f30250
shenfengjiao <shenfengjiao@100tal.com>
2020-08-10 15:41:56 +0800

    pub lib

---
+++ packages/button/src/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mo-button">test!</div>
+</template>
+
+<script>
+export default {
+  name: 'MoButton'
+};
+</script>
+
+<style lang="less">
+.mo-Button{
+  border: 1px solid black;
+  width: 100px;
+  height: 30px;
+  font-size: 30px;
+}
+</style>
